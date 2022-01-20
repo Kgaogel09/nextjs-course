12d6cc177e03f2d25674f0c17eac0e1c40ac8268
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-21 00:34:05 +0200

    refactor(components):handling events

--- react-refresher/src/components/Todo.js
+++ react-refresher/src/components/Todo.js
@@ -1,9 +1,14 @@
 function Todo({ title }) {
+  function deleteTodoHandle(e) {
+    console.log(e);
+  }
   return (
     <div className="card">
       <h2>{title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteTodoHandle}>
+          Delete
+        </button>
       </div>
     </div>
   );
