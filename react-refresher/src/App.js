b8710a79a302d049f6f10737c74a86e66e2c7ca9
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-21 00:20:30 +0200

    refactor(app):re-using components

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,13 +1,10 @@
+import Todo from "./components/Todo";
+
 export function App() {
   return (
     <div>
       <h1>My Todo's</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
     </div>
   );
 }
