87677c676c03e05c0f2ab9603e1237eab30231f9
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-21 15:07:26 +0200

    add(meetups):created new meetup form

--- react-router/src/pages/NewMeetUps.js
+++ react-router/src/pages/NewMeetUps.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetUps() {
-  return <div>New Meetups</div>;
+  return (
+    <section>
+      <h1>New Meetups</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetUps;
