31ea31f80e553a64eff66c515a2392bdcc4021e0
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-21 12:43:42 +0200

    add(meetups):mapped meetups data

---
+++ react-router/src/components/meetups/MeetupItem.js
@@ -0,0 +1,21 @@
+import classes from "./meetup-item.module.css";
+
+function MeetupItem({ image, title, address, description, id }) {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{title}</h3>
+        <address>{address}</address>
+        <p>{description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favorites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
