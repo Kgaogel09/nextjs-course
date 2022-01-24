a38747d6e3b025ad554b36efbdd7ea4c61cb2c84
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-24 09:49:30 +0200

    refactor(clients):navigating dynamic routes

--- nextjs-routing/pages/clients/index.js
+++ nextjs-routing/pages/clients/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Max" },
+    { id: "john", name: "John" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {/* <li>
+          <Link href="/clients/max">Max</Link>
+        </li>
+        <li>
+          <Link href="/clients/john">John</Link>
+        </li> */}
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
