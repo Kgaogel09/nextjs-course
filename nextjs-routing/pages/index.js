d35e4bcaa5345e3618bd60b7c0ccc018dbed2c0e
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-24 09:33:42 +0200

    refactor(homepage):used Link component to navigate

--- nextjs-routing/pages/index.js
+++ nextjs-routing/pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 function Homepage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          {/* sends a brand new request and gets a new html page */}
+          {/* <a href="/portfolio">Portfolio</a> */}
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
     </div>
   );
 }
