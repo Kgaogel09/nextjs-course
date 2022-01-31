e1e62a6a9dad613aa2e40e508f1c4b3d5f047c98
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-31 10:04:57 +0200

    refactor(homePage):created frontend form

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,5 +1,40 @@
+import { Fragment } from "react/cjs/react.development";
+import { useRef } from "react";
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const feedbackRef = emailRef.current.value;
+  }
+
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailRef}></input>
+        </div>
+        <br />
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea
+            rows="5"
+            type="feedback"
+            id="feedback"
+            ref={feedbackRef}
+          ></textarea>
+        </div>
+        <br />
+        <button onClick={submitFormHandler}>Send Feedback</button>
+      </form>
+    </Fragment>
+  );
 }
 
 export default HomePage;
