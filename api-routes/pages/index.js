4596a25b0d50ae59f39919d53b11c60b462152a4
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-01 14:37:51 +0200

    refactor(app):backend code with api

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,39 +1,60 @@
-import { Fragment } from "react/cjs/react.development";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function HomePage() {
-  const emailRef = useRef();
-  const feedbackRef = useRef();
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
 
   function submitFormHandler(event) {
     event.preventDefault();
 
-    const enteredEmail = emailRef.current.value;
-    const feedbackRef = emailRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
   }
 
   return (
-    <Fragment>
+    <div>
       <h1>The Home Page</h1>
       <form onSubmit={submitFormHandler}>
         <div>
-          <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" ref={emailRef}></input>
+          <label htmlFor="email">Your Email Address:</label> <br />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
-        <br />
         <div>
-          <label htmlFor="feedback">Your Feedback</label>
-          <textarea
-            rows="5"
-            type="feedback"
-            id="feedback"
-            ref={feedbackRef}
-          ></textarea>
+          <label htmlFor="feedback">Your Feedback:</label> <br />
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
-        <br />
-        <button onClick={submitFormHandler}>Send Feedback</button>
+        <button>Send Feedback</button>
       </form>
-    </Fragment>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
